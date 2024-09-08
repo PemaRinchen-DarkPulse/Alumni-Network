f6667e29d993ff41ecd3a52a27e5aac075f323d3
Pema Rinchen <pemarinchen.12.31.2002@gmail.com>
2024-09-08 21:49:24 +0530

    directory

---
+++ src/components/directory.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+
+function Directory() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default Directory;
