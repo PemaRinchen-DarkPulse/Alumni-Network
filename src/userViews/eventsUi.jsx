75098b1f48713ee62f1c928fb0a7886fcceeb21b
Pema Rinchen <pemarinchen.12.31.2002@gmail.com>
2024-09-22 23:50:39 +0530

    working on the Evnts page

--- src/userViews/eventsUi.jsx
+++ src/userViews/eventsUi.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from '../components/navbarFooter/header';
 import Footer from '../components/navbarFooter/footer';
+import {EventsList} from '../components/events/EventsList';
 
 const EventsUi = ({ isLoggedIn }) => {
   return (
     <>
       <Header isLoggedIn={isLoggedIn} />
-      <div style={{ height: "100vh" }}>
-        <h1 style={{ marginTop: "150px" }}>Events</h1>
-      </div>
+      <EventsList/>
       <Footer />
     </>
   );
