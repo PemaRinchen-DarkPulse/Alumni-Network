06bfe6be71d6e2414d60475d0a010e78f8d96f40
Pema Rinchen <pemarinchen.12.31.2002@gmail.com>
2024-09-26 22:59:57 +0530

    Made header and footer responsive

--- src/components/navbarFooter/footer.jsx
+++ src/components/navbarFooter/footer.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import "./style.css";
 function Footer(){
     return (
-        <footer>
-        <div className="container">
+        <footer >
             <p>&copy; 2024 Alumni Network. All rights reserved.</p>
             <p>Contact us at: <a href="mailto:info@alumninetwork.com">info@alumninetwork.com</a></p>
-        </div>
         </footer>
     );
 }
--- src/components/navbarFooter/header.jsx
+++ src/components/navbarFooter/header.jsx
@@ -1,86 +1,92 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
+import './style.css';
 
 const Header = ({ isLoggedIn }) => {
   const [isAlumniHubOpen, setAlumniHubOpen] = useState(false);
   const [isMentorshipOpen, setMentorshipOpen] = useState(false);
   const [isProfileDropdownOpen, setProfileDropdownOpen] = useState(false);
 
-  const toggleAlumniHubDropdown = (e) => {
-    e.preventDefault();
-    setAlumniHubOpen(!isAlumniHubOpen);
-    setMentorshipOpen(false); // Close mentorship dropdown if it's open
-    setProfileDropdownOpen(false); // Close profile dropdown if it's open
-  };
-
-  const toggleMentorshipDropdown = (e) => {
-    e.preventDefault();
-    setMentorshipOpen(!isMentorshipOpen);
-    setAlumniHubOpen(false); // Close alumni hub dropdown if it's open
-    setProfileDropdownOpen(false); // Close profile dropdown if it's open
-  };
-
   const toggleProfileDropdown = (e) => {
     e.preventDefault();
     setProfileDropdownOpen(!isProfileDropdownOpen);
-    setAlumniHubOpen(false); // Close alumni hub dropdown if it's open
-    setMentorshipOpen(false); // Close mentorship dropdown if it's open
+    setAlumniHubOpen(false);
+    setMentorshipOpen(false);
   };
 
   return (
-    <header>
+    <nav className="navbar navbar-expand-lg py-4 fixed-top z-index-5">
       <div className="container">
         <h1>Alumni Network</h1>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li className="directory">
-              <Link to="#" onClick={toggleAlumniHubDropdown} aria-expanded={isAlumniHubOpen}>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded={isAlumniHubOpen}>
                 Directory
-              </Link>
-              {isAlumniHubOpen && (
-                <div className="dropdown alumni-hub-dropdown">
-                  <Link to="/directory/alumni">Connect With Alumni</Link>
-                  <Link to="/directory/connection">My Connection</Link>
-                  <Link to="/directory/forum">Discussion Forum</Link>
-                </div>
-              )}
+              </a>
+              <ul className="dropdown-menu">
+                <li><Link to="/directory/alumni" className="dropdown-item">Connect With Alumni</Link></li>
+                <li><Link to="/directory/connection" className="dropdown-item">My Connection</Link></li>
+                <li><Link to="/directory/forum" className="dropdown-item">Discussion Forum</Link></li>
+              </ul>
             </li>
-            <li><Link to="/events">Events</Link></li>
-            <li className="directory">
-              <Link to="#" onClick={toggleMentorshipDropdown} aria-expanded={isMentorshipOpen}>
+
+            <li className="nav-item">
+              <Link to="/events" className="nav-link">Events</Link>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded={isMentorshipOpen}>
                 Mentorship
-              </Link>
-              {isMentorshipOpen && (
-                <div className="dropdown mentorship-dropdown">
-                  <Link to="/mentoring/becomeMentee">Find a Mentor</Link>
-                  <Link to="/mentoring/becomeMentor">Become a Mentor</Link>
+              </a>
+              <ul className="dropdown-menu">
+                <li><Link to="/mentoring/becomeMentee" className="dropdown-item">Find a Mentor</Link></li>
+                <li><Link to="/mentoring/becomeMentor" className="dropdown-item">Become a Mentor</Link></li>
+              </ul>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/news" className="nav-link">News</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/aboutus" className="nav-link">About Us</Link>
+            </li>
+
+            {isLoggedIn ? (
+              <div>
+                 <li className="nav-item dropdown profile-icon">
+              <a className="nav-link btn btn-link" onClick={toggleProfileDropdown} aria-expanded={isProfileDropdownOpen}>
+                <i className="fa fa-user"></i>
+              </a>
+              {isProfileDropdownOpen && (
+                <div className="dropdown-menu profile-dropdown show">
+                  <Link to="/settings" className="dropdown-item">Settings</Link>
+                  <Link to="/logout" className="dropdown-item">Log Out</Link>
                 </div>
               )}
             </li>
-            <li><Link to="/news">News</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/aboutus">About Us</Link></li>
-            {isLoggedIn ? (
-              <li className="profile-icon" onClick={toggleProfileDropdown}>
-                <Link to="#" aria-expanded={isProfileDropdownOpen}>
-                  <i className="fa fa-user"></i>
-                </Link>
-                {isProfileDropdownOpen && (
-                  <div className="dropdown profile-dropdown">
-                    <Link to="/settings">Settings</Link>
-                    <Link to="/logout">Log Out</Link>
-                  </div>
-                )}
-              </li>
+              </div>
+             
             ) : (
-              <li><Link to="/login">Login</Link></li>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">Login</Link>
+              </li>
             )}
           </ul>
-        </nav>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
